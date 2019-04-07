81343103238e7b2759b883ee3c9c522f278a5e4f
Louistiti <louis.grenard@gmail.com>
2019-04-07 15:38:28 +0800

    test(package/trend): Product Hunt module

---
+++ packages/trend/test/producthunt.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+describe('trend:producthunt', async () => {
+  test('requests Product Hunt', async () => {
+    global.nlu.brain.execute = jest.fn()
+    await global.nlu.process('What\'s trending on Product Hunt?')
+
+    const [obj] = global.nlu.brain.execute.mock.calls
+    await global.brain.execute(obj[0])
+
+    expect([
+      'reaching',
+      'today',
+      'unreachable'
+    ]).toIncludeAnyMembers(global.brain.finalOutput.codes)
+  })
+})
