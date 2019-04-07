32cc7dbe36592fb9618d9c10da5f05a4be7e41b6
Louistiti <louis.grenard@gmail.com>
2019-04-07 14:01:04 +0800

    feat(package/trend): basics done on the Product Hunt module

--- packages/trend/README.md
+++ packages/trend/README.md
@@ -14,13 +14,29 @@ Grab the GitHub trends repositories according to several options.
 (en-US) "What's trending on GitHub?"
 (en-US) "Give me the 4 GitHub trends of this week for the JavaScript language"
 (en-US) "What's the three GitHub trends of this month?"
+(fr-FR) "Quelles sont les tendances sur GitHub ?"
+(fr-FR) "Donne-moi les 4 tendances GitHub de cette semaine pour le langage JavaScript"
+(fr-FR) "Donne-moi les trois tendances GitHub de ce mois"
 ...
 ```
 
 ### Product Hunt
 
-WIP...
+Grab the Product Hunt trends products
 
 #### Usage
 
-WIP...
+1. Log in to your [Product Hunt](https://www.producthunt.com/) account.
+2. Add a [new application](https://www.producthunt.com/v1/oauth/applications) (E.g. name: Leon; Redirect URI: https://localhost).
+3. Once your application is created, click `Create Token`.
+4. Copy the `Developer Token` and paste it in `packages/trend/config/config.json` at the `producthunt.developer_token` key.
+
+```
+(en-US) "What's trending on Product Hunt?"
+(en-US) "Give me the 4 GitHub trends of this week for the JavaScript language"
+(en-US) "What's the three GitHub trends of this month?"
+(fr-FR) "Quelles sont les tendances sur GitHub ?"
+(fr-FR) "Donne-moi les 4 tendances GitHub de cette semaine pour le langage JavaScript"
+(fr-FR) "Donne-moi les trois tendances GitHub de ce mois"
+...
+```
--- packages/trend/producthunt.py
+++ packages/trend/producthunt.py
@@ -5,6 +5,60 @@ import requests
 import utils
 
 def producthunt(string, entities):
-	"""WIP..."""
+	"""Grab the Product Hunt trends"""
 
-	return utils.output('end', 'done')
+	# Developer token
+	developertoken = utils.config('developer_token')
+
+	# Number of products
+	limit = 5
+
+	# Answer key
+	answerkey = 'today'
+
+	for item in entities:
+		if item['entity'] == 'number':
+			limit = item['resolution']['value']
+
+	utils.output('inter', 'reaching', utils.translate('reaching'))
+
+	try:
+		r = utils.http('GET', 'https://api.producthunt.com/v1/posts', { 'Authorization': 'Bearer ' + developertoken })
+		posts = list(enumerate(r.json()['posts']))
+		result = ''
+
+		if limit > len(posts):
+			utils.output('inter', 'limit_max', utils.translate('limit_max', {
+			  'limit': limit,
+			  'new_limit': len(posts)
+			}))
+			limit = len(posts)
+		elif limit == 0:
+			limit = 5
+
+		for i, post in posts:
+			author = { 'profile_url': '?', 'name': '?' }
+			if post['maker_inside']:
+				author = list(reversed(post['makers']))[0]
+
+			result += utils.translate('list_element', {
+						'rank': i + 1,
+						'post_url': post['discussion_url'],
+						'product_name': post['name'],
+						'author_url': author['profile_url'],
+						'author_name': author['name'],
+						'votes_nb': post['votes_count']
+					}
+				)
+
+			if (i + 1) == limit:
+				break
+
+		return utils.output('end', answerkey, utils.translate(answerkey, {
+					'limit': limit,
+					'result': result
+				}
+			)
+		)
+	except requests.exceptions.RequestException as e:
+		return utils.output('end', 'unreachable', utils.translate('unreachable'))
