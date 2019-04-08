85b582491df0a68a2bc5575aa566331b07c61a81
bob365 <liu.bosen@qq.com>
2019-04-08 16:12:01 +0800

    update trained to loaded

--- scripts/train.js
+++ scripts/train.js
@@ -53,7 +53,7 @@ export default () => new Promise(async (resolve, reject) => {
             manager.addDocument(lang, exprs[k], `${packages[i]}:${modules[j]}`)
           }
 
-          log.success(`"${string.ucfirst(modules[j])}" module expressions loaded«`)
+          log.success(`"${string.ucfirst(modules[j])}" module expressions loaded`)
         }
       }
 
