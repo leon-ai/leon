a476fd0f38f18bf8035db213be2c55f83871038d
Louistiti <louis.grenard@gmail.com>
2019-04-27 12:12:04 +0800

    feat(package/checker): allow multiple breaches output in the Have I Been Pwned module

--- packages/checker/haveibeenpwned.py
+++ packages/checker/haveibeenpwned.py
@@ -8,16 +8,17 @@ from requests import codes, exceptions
 
 def haveibeenpwned(string, entities):
     emails = []
-    
+      
     for item in entities:
         if item['entity'] == 'email':
             emails.append(item['resolution']['value'])
-    
+
     if not emails:
         emails = utils.config('emails')
+
         if not emails:
             return utils.output('end', 'no-email', utils.translate('no-email'))
-    
+
     utils.output('inter', 'checking', utils.translate('checking'))
 
     for index, email in enumerate(emails):
@@ -25,16 +26,25 @@ def haveibeenpwned(string, entities):
         breached = checkForBreach(email)
         data = { 'email': email }
 
-        # HaveIBeenPwned API returns a 403 when accessed by unauthorized/banned clients
+        # Have I Been Pwned API returns a 403 when accessed by unauthorized/banned clients
         if breached == 403:
-            return utils.output('end', 'blocked', utils.translate('blocked', { 'website_name': 'HaveIBeenPwned' }))
+            return utils.output('end', 'blocked', utils.translate('blocked', { 'website_name': 'Have I Been Pwned' }))
         elif not breached:
             if isLastEmail:
                 return utils.output('end', 'no-pwnage', utils.translate('no-pwnage', data))
             else:
                 utils.output('inter', 'no-pwnage', utils.translate('no-pwnage', data))
         else:
-            data['breach'] = breached[0]['Name']
+            data['result'] = ''
+
+            for index, b in enumerate(breached):
+                data['result'] += utils.translate('list_element', {
+                        'url': 'http://' + b['Domain'],
+                        'name': b['Name'],
+                        'total': b['PwnCount']
+                    }
+                )
+
             if isLastEmail:
                 return utils.output('end', 'pwned', utils.translate('pwned', data))
             else:
@@ -44,16 +54,16 @@ def checkForBreach(email):
     # Delay for 2 seconds before making request to accomodate API usage policy
     sleep(2)
     truncate = '?truncateResponse=true'
-    url = 'https://haveibeenpwned.com/api/v2/breachedaccount/' + parse.quote_plus(email) + truncate
-    
+    url = 'https://haveibeenpwned.com/api/v2/breachedaccount/' + parse.quote_plus(email)
+
     try:
         response = utils.http('GET', url)
 
         if response.status_code == 404:
             return None
         elif response.status_code == 200:
-            return response.json
-        
+            return response.json()
+
         return response.status_code
     except exceptions.RequestException as e:
-        return utils.output('end', 'down', utils.translate('errors', { 'website_name': 'HaveIBeenPwned' }))
+        return utils.output('end', 'down', utils.translate('errors', { 'website_name': 'Have I Been Pwned' }))
--- packages/checker/test/haveibeenpwned.spec.js
+++ packages/checker/test/haveibeenpwned.spec.js
@@ -1,9 +1,9 @@
 'use strict'
 
 describe('checker:haveibeenpwned', async () => {
-  test('checks that an email has been pwned', async () => {
+  test('checks if an email address has been pwned', async () => {
     global.nlu.brain.execute = jest.fn()
-    await global.nlu.process('Has iifeoluwa.ao@gmail.com been pwned?')
+    await global.nlu.process('Has supercleanemailaddress@supercleandomainname.com been pwned?')
 
     const [obj] = global.nlu.brain.execute.mock.calls
     await global.brain.execute(obj[0])
@@ -11,9 +11,9 @@ describe('checker:haveibeenpwned', async () => {
     expect(global.brain.finalOutput.codes).toIncludeSameMembers(['checking', 'no-pwnage'])
   })
 
-  test('checks that multiple emails have been pwned', async () => {
+  test('checks if multiple email addresses have been pwned', async () => {
     global.nlu.brain.execute = jest.fn()
-    await global.nlu.process('Verify the pwnage status of iifeoluwa.ao@gmail.com and ifeoluwa1990@yahoo.com')
+    await global.nlu.process('Verify the pwnage status of supercleanemailaddress@supercleandomainname.com and test@toto.com')
 
     const [obj] = global.nlu.brain.execute.mock.calls
     await global.brain.execute(obj[0])
