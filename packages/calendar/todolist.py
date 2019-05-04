0cdd73d6c24a287915f691e3b12edacd75fd383a
Louistiti <louis.grenard@gmail.com>
2019-05-04 09:26:11 +0800

    feat(package/calendar): To-Do List module backbone

--- packages/calendar/todolist.py
+++ packages/calendar/todolist.py
@@ -7,20 +7,41 @@ import utils
 def create_list(string, entities):
 	"""WIP"""
 
-	return utils.output('end', 'list_created', utils.translate('list_created', { 'list': 'test' }))
+	return utils.output('end', 'list_created', utils.translate('list_created', { 'list': 'fake' }))
+
+def rename_list(string, entities):
+	"""WIP"""
+
+	return utils.output('end', 'list_renamed', utils.translate('list_renamed', {
+	  'old_list': 'fake',
+	  'new_list': 'new'
+	}))
+
+def delete_list(string, entities):
+	"""WIP"""
+
+	return utils.output('end', 'list_deleted', utils.translate('list_deleted', { 'list': 'fake' }))
 
 def add_todo(string, entities):
 	"""WIP"""
 
 	return utils.output('end', 'todo_added', utils.translate('todo_added', {
-	  'list': 'test',
+	  'list': 'fake',
 	  'todo': 'todo 1'
 	}))
 
-def delete_todo(string, entities):
+def complete_todo(string, entities):
 	"""WIP"""
 
-	return utils.output('end', 'todo_deleted', utils.translate('todo_deleted', {
-	  'list': 'test',
+	return utils.output('end', 'todo_completed', utils.translate('todo_completed', {
+	  'list': 'fake',
+	  'todo': 'todo 1'
+	}))
+
+def archive_todo(string, entities):
+	"""WIP"""
+
+	return utils.output('end', 'todo_archived', utils.translate('todo_archived', {
+	  'list': 'fake',
 	  'todo': 'todo 1'
 	}))
