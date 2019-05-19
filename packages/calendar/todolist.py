857be947792c650ac35847e14fc41064008cef24
Louistiti <louis.grenard@gmail.com>
2019-05-19 11:53:26 +0800

    feat(package/calendar): create a list in the To-Do List module

--- packages/calendar/todolist.py
+++ packages/calendar/todolist.py
@@ -3,11 +3,44 @@
 
 import requests
 import utils
+from time import time
+
+# Package database
+db = utils.db()['db']
+
+# Lists of the module table
+lists = db.table('todo_lists')
+
+# Query
+List = utils.db()['query']()
 
 def create_list(string, entities):
-	"""WIP"""
+	"""Create a to-do list"""
 
-	return utils.output('end', 'list_created', utils.translate('list_created', { 'list': 'fake' }))
+	# List name
+	listname = ''
+
+	# Find entities
+	for item in entities:
+		if item['entity'] == 'list':
+			listname = item['sourceText']
+
+	# Verify if a list name has been provided
+	if not listname:
+		return utils.output('end', 'list_name_not_provided', utils.translate('list_name_not_provided'))
+
+	# Verify if list already exists or not
+	if lists.count(List.name == listname) > 0:
+		return utils.output('end', 'list_already_exists', utils.translate('list_already_exists', { 'list': listname }))
+
+	# Create the new to-do list
+	lists.insert({
+		'name': listname,
+		'todos': [],
+		'created_at': int(time())
+	})
+
+	return utils.output('end', 'list_created', utils.translate('list_created', { 'list': listname }))
 
 def rename_list(string, entities):
 	"""WIP"""
