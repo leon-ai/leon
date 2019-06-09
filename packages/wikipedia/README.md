070e853dc4e3a17899dcb91b36e7435b28bd1de3
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2019-06-09 23:07:52 +0200

    feat(package/wikipedia): Initial commit. Unstable.

---
+++ packages/wikipedia/README.md
@@ -0,0 +1,28 @@
+# Wikipedia Package
+
+The Wikipedia package allows your personal assistant to gather pieces of information from Wikipedia.
+
+#### Requirements
+- wikipedia
+
+#### Usage
+
+1. Duplicate the file `packages/wikipedia/config/config.sample.json` and rename it `config.json`.
+2. Set your language.
+3. This package uses wikipedia. To install it: from inside leon directory `cd bridges/python`
+4. `pipenv install wikipedia`
+5. Done!
+
+```
+(en-US)
+- "Search on Wikipedia GitHub"
+```
+
+#### Options
+- `lang`: Choose your language. Default: `en`
+
+#### Links
+
+- [MediaWiki API](https://www.mediawiki.org/wiki/API:Main_page)
+- [Wikipedia GitHub](https://github.com/goldsmith/Wikipedia)
+- [Wikipedia Docs](https://wikipedia.readthedocs.io/en/latest/)
---
+++ packages/wikipedia/wikipedia.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
+
+import utils
+
+wikipedia.set_lang(utils.config("lang"))
+
+def summary(string, entities):
+    """
+    Get the summary of the requested wikipedia page.
+    """
+
+    for entity in entities:
+        if entity["entity"] == "page":
+            utils.output("inter", "acquiring", utils.translate("acquiring"))
+
+            try:
+                summary = wikipedia.summary(entity["sourceText"])
+                return utils.output("end", "summary", utils.translate("summary", {"summary": summary}))
+
+            except DisambiguationError as pages:
+                # TODO: ask which page to visit
+                return utils.output("end", "disambiguation_error", utils.translate("disambiguation_error"))
+
+            except PageError:
+                return utils.output("end", "page_error", utils.translate("page_error"))
+
+    return utils.output("end", "no_entities_error", utils.translate("no_entities_error"))
