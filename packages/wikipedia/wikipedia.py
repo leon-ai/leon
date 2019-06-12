4809aa35a7f586a8e116627d6c494df9cae103f0
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2019-06-12 20:43:32 +0200

    refactor(package/wikipedia): Refactor code to fulfill PEP8

--- packages/wikipedia/wikipedia.py
+++ packages/wikipedia/wikipedia.py
@@ -8,6 +8,7 @@ import utils
 
 wikipedia.set_lang(utils.config("lang"))
 
+
 def summary(string, entities):
     """
     Get the summary of the requested wikipedia page.
@@ -15,17 +16,43 @@ def summary(string, entities):
 
     for entity in entities:
         if entity["entity"] == "page":
-            utils.output("inter", "acquiring", utils.translate("acquiring"))
+            utils.output(
+                "inter",
+                "acquiring",
+                utils.translate("acquiring")
+            )
 
             try:
-                summary = wikipedia.summary(entity["sourceText"], sentences=utils.config("sentences"))
-                return utils.output("end", "summary", utils.translate("summary", {"summary": summary}))
+                summary = wikipedia.summary(
+                    entity["sourceText"],
+                    sentences=utils.config("sentences")
+                )
+                return utils.output(
+                    "end",
+                    "summary",
+                    utils.translate(
+                        "summary",
+                        {"summary": summary}
+                    )
+                )
 
             except DisambiguationError as pages:
                 # TODO: ask which page to visit
-                return utils.output("end", "disambiguation_error", utils.translate("disambiguation_error"))
+                return utils.output(
+                    "end",
+                    "disambiguation_error",
+                    utils.translate("disambiguation_error")
+                )
 
             except PageError:
-                return utils.output("end", "page_error", utils.translate("page_error"))
+                return utils.output(
+                    "end",
+                    "page_error",
+                    utils.translate("page_error")
+                )
 
-    return utils.output("end", "no_entities_error", utils.translate("no_entities_error"))
+    return utils.output(
+        "end",
+        "no_entities_error",
+        utils.translate("no_entities_error")
+    )
