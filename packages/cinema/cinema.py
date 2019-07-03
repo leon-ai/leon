d343406926300fd78cc09c5556e13573ea1f3d83
gnouf1 <jeuxforum26@gmail.com>
2019-07-03 23:26:26 +0200

    info tv + movie | discover serie

--- packages/cinema/cinema.py
+++ packages/cinema/cinema.py
@@ -6,6 +6,7 @@ import tmdbsimple as tmdb
 import functools
 import random as ran
 import datetime as dt
+import json
 
 # Package database
 db = utils.db()['db']
@@ -27,6 +28,12 @@ def load_config(func):
         payload["API_KEY"] = utils.config('API_KEY')
         tmdb.API_KEY = payload["API_KEY"]
 
+        with open('packages/cinema/data/words/movie.json') as json_data:
+            payload["trl_movie"] = json.load(json_data)
+        with open('packages/cinema/data/words/serie.json') as json_data:
+            payload["trl_serie"] = json.load(json_data)
+
+
         return func(payload)
 
     return wrapper_load_config
@@ -34,30 +41,75 @@ def load_config(func):
 
 @load_config
 def recommend(payload):
-    if db.search(Query.type == 'rmovie_request'):
-        payload["movie"] = db.search(Query.type == 'rmovie_request')[0]['content']
-        o_id = db.search(Query.type == 'rmovie_request')[0]['old_id']
-        o_id = o_id + 1
-        db.update({"old_id": o_id}, Query.type == 'rmovie_request')
-        db.remove(Query.type == 'rmovie_request' and Query.old_id > 10)
-    else:
-        # Get an random movie with user's language
-        payload["movie"] = tmdb.Discover().movie(
-        page=ran.randint(0, 1000),
-        language=payload["lang"],
-        vote_average_gte=5.00)
-        db.insert({'type': 'rmovie_request','old_id': 0, 'content': payload["movie"]})
+    flag_movie = 0
+    flag_serie = 0
+    for item in payload["entities"]:
+        if item["entity"] == "Serie_Or_Movie":
+            word = item["sourceText"]
+            i = 0
+            wflag = 0
+            while i < 92 and wflag == 0:
+                if payload["trl_movie"][i]["string"] == word:
+                    flag_movie = 1
+                    wflag = 1
+                elif payload["trl_serie"][i]["string"] == word:
+                    flag_serie = 1
+                    wflag = 1
+                i = i + 1
+        else:
+            return utils.output('end', 'Error', utils.translate('Error'))
 
-    movie_nO = ran.randint(0, 20)
-    movie = payload["movie"]["results"][movie_nO]
-    movie_title = movie["title"]
-    movie_rdate = movie["release_date"]
-    movie_sum = movie["overview"]
+    if flag_movie == 1:
+        if db.search(Query.type == 'rmovie_request'):
+            payload["movie"] = db.search(Query.type == 'rmovie_request')[0]['content']
+            o_id = db.search(Query.type == 'rmovie_request')[0]['old_id']
+            o_id = o_id + 1
+            db.update({"old_id": o_id}, Query.type == 'rmovie_request')
+            db.remove(Query.type == 'rmovie_request' and Query.old_id > 10)
+        else:
+            # Get an random movie with user's language
+            payload["movie"] = tmdb.Discover().movie(
+            page=ran.randint(0, 1000),
+            language=payload["lang"],
+            vote_average_gte=7.00)
+            db.insert({'type': 'rmovie_request','old_id': 0, 'content': payload["movie"]})
 
-    return utils.output('end', 'recommend', utils.translate('recommend', {
-    "movie_title": movie_title,
-    "release_date": movie_rdate,
-    "summarize": movie_sum}))
+        movie_nO = ran.randint(0, 20)
+        movie = payload["movie"]["results"][movie_nO]
+        movie_title = movie["title"]
+        movie_rdate = movie["release_date"]
+        movie_sum = movie["overview"]
+
+        return utils.output('end', 'recommend', utils.translate('recommend', {
+        "movie_title": movie_title,
+        "release_date": movie_rdate,
+        "summarize": movie_sum}))
+
+    elif flag_serie == 1:
+        if db.search(Query.type == 'rserie_request'):
+            payload["serie"] = db.search(Query.type == 'rserie_request')[0]['content']
+            o_id = db.search(Query.type == 'rserie_request')[0]['old_id']
+            o_id = o_id + 1
+            db.update({"old_id": o_id}, Query.type == 'rserie_request')
+            db.remove(Query.type == 'rserie_request' and Query.old_id > 10)
+        else:
+            # Get an random serie with user's language
+            payload["serie"] = tmdb.Discover().tv(
+            page=ran.randint(0, 1000),
+            language=payload["lang"],
+            vote_average_gte=7.00)
+            db.insert({'type': 'rserie_request','old_id': 0, 'content': payload["movie"]})
+
+        serie_nO = ran.randint(0, 20)
+        serie = payload["serie"]["results"][serie_nO]
+        serie_title = serie["name"]
+        serie_rdate = serie["first_air_date"]
+        serie_sum = serie["overview"]
+
+        return utils.output('end', 'recommend', utils.translate('recommend', {
+        "movie_title": serie_title,
+        "release_date": serie_rdate,
+        "summarize": serie_sum}))
 
 
 @load_config
@@ -73,3 +125,19 @@ def now_theatres(payload):
     return utils.output("end", "nit_title_list", utils.translate('nit_list', {
     "nit_title": res
     }))
+
+@load_config
+def info(payload):
+    for item in payload["entities"]:
+        if item["entity"] == "title":
+            data_title = item["sourceText"]
+
+    data_info = tmdb.Search().collection(query=data_title, language=payload["lang"])
+    if data_info["total_results"] == 0:
+            data_info = tmdb.Search().multi(query=data_title, language=payload["lang"])
+
+    data_overview = data_info["results"][0]["overview"]
+
+    return utils.output("end", "info", utils.translate('info', {
+    "title": data_title.title(),
+    "overview": data_overview}))
