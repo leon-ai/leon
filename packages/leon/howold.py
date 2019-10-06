d13cb084770f5fa79ed74a9fc4237871eb5398e1
Francois <asteryujano@gmail.com>
2019-10-06 12:34:20 +0200

    feat(package/leon): complete the date of creation witht age (years and days).

--- packages/leon/howold.py
+++ packages/leon/howold.py
@@ -2,8 +2,16 @@
 # -*- coding:utf-8 -*-
 
 import utils
+import datetime
 
 def run(string, entities):
 	"""Leon says when it was created"""
 
-	return utils.output('end', 'age', utils.translate('age'))
+	age = datetime.date.today() - datetime.date(2019, 2, 10)
+
+	data = {
+		'years': int(age.days / 365),
+		'days': (age.days % 365)
+	}
+
+	return utils.output('end', 'age', utils.translate('age', data))
