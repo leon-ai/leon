387092001409711807fd810033abc3f1233425fb
Krorin <krorin@live.com>
2020-03-16 11:45:25 +0100

    feat(package/math): first commit

--- packages/math/calculator.py
+++ packages/math/calculator.py
@@ -2,4 +2,11 @@
 from bridges.python import utils
 
 def run(string, entities):
-  pass
+  
+  #equation = entities['sourceText']
+
+  #results = entities[0]['entity']
+
+  #results = eval(equation)
+
+  return utils.output('end', 'results', 'results')
