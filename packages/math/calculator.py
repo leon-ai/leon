c1dd2000f61b7ab5c74b42d171e616879f17362f
Krorin <krorin@live.com>
2020-03-17 02:41:20 +0100

    feat(package/math): Added functionality to percentage and calc

--- packages/math/calculator.py
+++ packages/math/calculator.py
@@ -2,11 +2,48 @@
 from bridges.python import utils
 
 def run(string, entities):
-  
-  #equation = entities['sourceText']
+  try:
+    numberText = []
+    numberList = []
+    operatorList = []
 
-  #results = entities[0]['entity']
+    for x in entities:
+      numberText.append(x['sourceText'])
 
-  #results = eval(equation)
+    #looks for operators and takes them out
+    for x in range(len(numberText)-1):
+      number1 = string.find(entities[x]['sourceText'])
+      number2 = string.find(entities[x+1]['sourceText'], number1+1)
+      operator = string[number1+len(entities[x]['sourceText']):number2]
+      operatorList.append(operator.strip())
 
-  return utils.output('end', 'results', 'results')
+    #converts operators
+    for x in range(len(operatorList)):
+      if(operatorList[x].lower() == "plus"):
+        operatorList[x] = "+"
+      if(operatorList[x].lower() == "minus"):
+        operatorList[x] = "-"
+      if(operatorList[x].lower() == "divided by"):
+        operatorList[x] = "/"
+      if(operatorList[x].lower() == "multiplied by"):
+        operatorList[x] = "*"
+
+    #makes the equation string
+    equation = ""
+    for x in range(len(entities)-1):
+      if(entities[x+1]['entity'] == 'date'):
+        equation += str(entities[x+1]['sourceText'])
+      else:
+        if(x == 0):
+          equation = str(entities[x]['resolution']['value']) + \
+              operatorList[x] + str(entities[x+1]['resolution']['value'])
+        else:
+          equation += operatorList[x] + str(entities[x+1]['resolution']['value'])
+
+    #calculates the equation
+    results = eval(equation)
+
+    return utils.output('end', 'test', utils.translate('result', {'equation': equation, 'results': results}))
+
+  except:
+    return utils.output('end', 'test', utils.translate('error'))
--- packages/math/percentage.py
+++ packages/math/percentage.py
@@ -2,4 +2,15 @@
 from bridges.python import utils
 
 def run(string, entities):
-  pass
+  try:
+    percent = entities[0]['resolution']['value']
+    value = entities[1]['resolution']['value']
+
+    percent = percent/100
+
+    result = percent * value
+
+    return utils.output('end', 'test', utils.translate('result', {'result': result}))
+  except:
+    return utils.output('end', 'test', utils.translate('error'))
+    
