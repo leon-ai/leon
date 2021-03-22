71f47ef898935e5b4de5cfdf54d9c2cb063074ab
divlo <contact@divlo.fr>
2021-03-22 20:23:23 +0100

    chore: migrate from express to fastify

---
+++ server/src/api/downloads/get.js
@@ -0,0 +1,127 @@
+import fs from 'fs'
+import archiver from 'archiver'
+
+import log from '@/helpers/log'
+import string from '@/helpers/string'
+
+const getDownloads = async (fastify, options) => {
+  fastify.get(`/${options.apiVersion}/downloads`, (request, reply) => {
+    log.title('GET /downloads')
+
+    const clean = (dir, files) => {
+      log.info('Cleaning module download directory...')
+      for (let i = 0; i < files.length; i += 1) {
+        fs.unlinkSync(`${dir}/${files[i]}`)
+      }
+      fs.rmdirSync(dir)
+      log.success('Downloads directory cleaned')
+    }
+    let message = ''
+
+    if (request.query.package && request.query.module) {
+      const packageDir = `${__dirname}/../../../../packages/${request.query.package}`
+      const dlPackageDir = `${__dirname}/../../../../downloads/${request.query.package}`
+      const module = `${packageDir}/${request.query.module}.py`
+
+      log.info(
+        `Checking existence of the ${string.ucfirst(
+          request.query.module
+        )} module...`
+      )
+      if (fs.existsSync(module)) {
+        log.success(`${string.ucfirst(request.query.module)} module exists`)
+        const downloadsDir = `${dlPackageDir}/${request.query.module}`
+
+        log.info('Reading downloads directory...')
+        fs.readdir(downloadsDir, (err, files) => {
+          if (err && err.code === 'ENOENT') {
+            message = 'There is no content to download for this module.'
+            log.error(message)
+            reply.send({
+              success: false,
+              status: 404,
+              code: 'module_dir_not_found',
+              message
+            })
+          } else {
+            if (err) log.error(err)
+
+            // Download the file if there is only one
+            if (files.length === 1) {
+              log.info(`${files[0]} is downloading...`)
+              reply.download(`${downloadsDir}/${files[0]}`)
+              log.success(`${files[0]} downloaded`)
+              clean(downloadsDir, files)
+            } else {
+              log.info('Deleting previous archives...')
+              const zipSlug = `leon-${request.query.package}-${request.query.module}`
+              const pkgFiles = fs.readdirSync(dlPackageDir)
+
+              for (let i = 0; i < pkgFiles.length; i += 1) {
+                if (
+                  pkgFiles[i].indexOf('.zip') !== -1
+                  && pkgFiles[i].indexOf(zipSlug) !== -1
+                ) {
+                  fs.unlinkSync(`${dlPackageDir}/${pkgFiles[i]}`)
+                  log.success(`${pkgFiles[i]} archive deleted`)
+                }
+              }
+
+              log.info('Preparing new archive...')
+              const zipName = `${zipSlug}-${Date.now()}.zip`
+              const zipFile = `${dlPackageDir}/${zipName}`
+              const output = fs.createWriteStream(zipFile)
+              const archive = archiver('zip', { zlib: { level: 9 } })
+
+              // When the archive is ready
+              output.on('close', () => {
+                log.info(`${zipName} is downloading...`)
+                reply.download(zipFile, (err) => {
+                  if (err) log.error(err)
+
+                  log.success(`${zipName} downloaded`)
+
+                  clean(downloadsDir, files)
+                })
+              })
+              archive.on('error', (err) => {
+                log.error(err)
+              })
+
+              // Add the content to the archive
+              log.info('Adding content...')
+              archive.directory(downloadsDir, false)
+
+              // Inject stream data to the archive
+              log.info('Injecting stream data...')
+              archive.pipe(output)
+
+              log.info('Finalizing...')
+              archive.finalize()
+            }
+          }
+        })
+      } else {
+        message = 'This module does not exist.'
+        log.error(message)
+        reply.send({
+          success: false,
+          status: 404,
+          code: 'module_not_found',
+          message
+        })
+      }
+    } else {
+      message = 'Bad request.'
+      log.error(message)
+      reply.send({
+        success: false,
+        status: 400,
+        code: 'bad_request',
+        message
+      })
+    }
+  })
+}
+
+export default getDownloads
--- server/src/core/server.js
+++ server/src/core/server.js
@@ -1,6 +1,5 @@
-import express from 'express'
-import http from 'http'
-import bodyParser from 'body-parser'
+import Fastify from 'fastify'
+import fastifyStatic from 'fastify-static'
 import socketio from 'socket.io'
 import path from 'path'
 
@@ -10,14 +9,14 @@ import Nlu from '@/core/nlu'
 import Brain from '@/core/brain'
 import Asr from '@/core/asr'
 import Stt from '@/stt/stt'
-import corsMidd from '@/middleware/cors'
-import otherMidd from '@/middleware/other'
-import infoRouter from '@/api/info/info.routes'
-import downloadRouter from '@/api/downloads/download.routes'
+import corsMidd from '@/plugins/cors'
+import otherMidd from '@/plugins/other'
+import infoPlugin from '@/api/info/index'
+import downloadsPlugin from '@/api/downloads/index'
 import log from '@/helpers/log'
 import date from '@/helpers/date'
 
-const app = express()
+const fastify = Fastify()
 let stt = { }
 
 class Server {
@@ -28,162 +27,133 @@ class Server {
   /**
    * Server entry point
    */
-  static init () {
-    return new Promise(async (resolve) => {
-      // CORS middleware
-      app.use(corsMidd)
+  static async init () {
+    fastify.addHook('onRequest', corsMidd)
+    fastify.addHook('onRequest', otherMidd)
 
-      // A simple middleware
-      app.use(otherMidd)
+    log.title('Initialization')
+    log.success(`The current env is ${process.env.LEON_NODE_ENV}`)
+    log.success(`The current version is ${version}`)
 
-      app.use(bodyParser.json())
-      app.use(bodyParser.urlencoded({
-        extended: true
-      }))
+    if (!Object.keys(langs).includes(process.env.LEON_LANG) === true) {
+      process.env.LEON_LANG = 'en-US'
+      log.warning('The language you chose is not supported, then the default language has been applied')
+    }
 
-      log.title('Initialization')
-      log.success(`The current env is ${process.env.LEON_NODE_ENV}`)
-      log.success(`The current version is ${version}`)
+    log.success(`The current language is ${process.env.LEON_LANG}`)
+    log.success(`The current time zone is ${date.timeZone()}`)
 
-      if (!Object.keys(langs).includes(process.env.LEON_LANG) === true) {
-        process.env.LEON_LANG = 'en-US'
+    const sLogger = (process.env.LEON_LOGGER !== 'true') ? 'disabled' : 'enabled'
+    log.success(`Collaborative logger ${sLogger}`)
 
-        log.warning('The language you chose is not supported, then the default language has been applied')
-      }
-
-      log.success(`The current language is ${process.env.LEON_LANG}`)
-      log.success(`The current time zone is ${date.timeZone()}`)
-
-      const sLogger = (process.env.LEON_LOGGER !== 'true') ? 'disabled' : 'enabled'
-      log.success(`Collaborative logger ${sLogger}`)
-
-      await this.bootstrap()
-      resolve()
-    })
+    await Server.bootstrap()
   }
 
   /**
    * Bootstrap API
    */
-  static bootstrap () {
-    return new Promise(async (resolve) => {
-      const apiVersion = 'v1'
+  static async bootstrap () {
+    const apiVersion = 'v1'
 
-      // Render the web app
-      app.use(express.static(`${__dirname}/../../../app/dist`))
-      app.get('/', (req, res) => {
-        res.sendFile(path.resolve(`${__dirname}/../../../app/dist/index.html`))
-      })
-
-      app.use(`/${apiVersion}/info`, infoRouter)
-      app.use(`/${apiVersion}/downloads`, downloadRouter)
-
-      try {
-        this.server = http.createServer(app)
-
-        await this.listen(process.env.LEON_PORT)
-        resolve()
-      } catch (e) {
-        log[e.type](e.obj.message)
-      }
+    // Render the web app
+    fastify.register(fastifyStatic, {
+      root: path.join(__dirname, '..', '..', '..', 'app', 'dist'),
+      prefix: '/'
     })
+    fastify.get('/', (_request, reply) => {
+      reply.sendFile('index.html')
+    })
+
+    fastify.register(infoPlugin, { apiVersion })
+    fastify.register(downloadsPlugin, { apiVersion })
+    this.server = fastify.server
+    try {
+      await Server.listen(process.env.LEON_PORT)
+    } catch (e) {
+      log.error(e.message)
+    }
   }
 
   /**
    * Launch server
    */
-  static listen (port) {
-    return new Promise((resolve, reject) => {
-      const io = process.env.LEON_NODE_ENV === 'development'
-        ? socketio(this.server, { cors: { origin: `${process.env.LEON_HOST}:3000` } })
-        : socketio(this.server)
+  static async listen (port) {
+    const io = process.env.LEON_NODE_ENV === 'development'
+      ? socketio(this.server, { cors: { origin: `${process.env.LEON_HOST}:3000` } })
+      : socketio(this.server)
 
-      io.on('connection', this.connection)
+    io.on('connection', Server.connection)
 
-      this.server.listen(port, (err) => {
-        /* istanbul ignore if */
-        if (err) {
-          reject({ type: 'error', obj: err })
-          return
-        }
-
-        log.success(`Server is available at ${process.env.LEON_HOST}:${port}`)
-
-        resolve()
-      })
-    })
+    await fastify.listen(port, '0.0.0.0')
+    log.success(`Server is available at ${process.env.LEON_HOST}:${port}`)
   }
 
   /**
    * Bootstrap socket
    */
-  static connection (socket) {
-    return new Promise((resolve) => {
-      log.title('Client')
-      log.success('Connected')
+  static async connection (socket) {
+    log.title('Client')
+    log.success('Connected')
 
-      // Init
-      socket.on('init', async (data) => {
-        log.info(`Type: ${data}`)
-        log.info(`Socket id: ${socket.id}`)
+    // Init
+    socket.on('init', async (data) => {
+      log.info(`Type: ${data}`)
+      log.info(`Socket id: ${socket.id}`)
 
-        if (data === 'hotword-node') {
-          // Hotword triggered
-          socket.on('hotword-detected', (data) => {
-            log.title('Socket')
-            log.success(`Hotword ${data.hotword} detected`)
+      if (data === 'hotword-node') {
+        // Hotword triggered
+        socket.on('hotword-detected', (data) => {
+          log.title('Socket')
+          log.success(`Hotword ${data.hotword} detected`)
 
-            socket.broadcast.emit('enable-record')
-          })
-        } else {
-          const brain = new Brain(socket, langs[process.env.LEON_LANG].short)
-          const nlu = new Nlu(brain)
-          const asr = new Asr()
-          let sttState = 'disabled'
-          let ttsState = 'disabled'
+          socket.broadcast.emit('enable-record')
+        })
+      } else {
+        const brain = new Brain(socket, langs[process.env.LEON_LANG].short)
+        const nlu = new Nlu(brain)
+        const asr = new Asr()
+        let sttState = 'disabled'
+        let ttsState = 'disabled'
 
-          if (process.env.LEON_STT === 'true') {
-            sttState = 'enabled'
+        if (process.env.LEON_STT === 'true') {
+          sttState = 'enabled'
 
-            stt = new Stt(socket, process.env.LEON_STT_PROVIDER)
-            stt.init()
-          }
-          if (process.env.LEON_TTS === 'true') {
-            ttsState = 'enabled'
-          }
+          stt = new Stt(socket, process.env.LEON_STT_PROVIDER)
+          stt.init()
+        }
+        if (process.env.LEON_TTS === 'true') {
+          ttsState = 'enabled'
+        }
 
-          log.title('Initialization')
-          log.success(`STT ${sttState}`)
-          log.success(`TTS ${ttsState}`)
+        log.title('Initialization')
+        log.success(`STT ${sttState}`)
+        log.success(`TTS ${ttsState}`)
 
-          // Train modules expressions
+        // Train modules expressions
+        try {
+          await nlu.loadModel(`${__dirname}/../data/expressions/classifier.json`)
+        } catch (e) {
+          log[e.type](e.obj.message)
+        }
+
+        // Listen for new query
+        socket.on('query', async (data) => {
+          log.title('Socket')
+          log.info(`${data.client} emitted: ${data.value}`)
+
+          socket.emit('is-typing', true)
+          await nlu.process(data.value)
+        })
+
+        // Handle automatic speech recognition
+        socket.on('recognize', async (data) => {
           try {
-            await nlu.loadModel(`${__dirname}/../data/expressions/classifier.json`)
+            await asr.run(data, stt)
           } catch (e) {
             log[e.type](e.obj.message)
           }
-
-          // Listen for new query
-          socket.on('query', async (data) => {
-            log.title('Socket')
-            log.info(`${data.client} emitted: ${data.value}`)
-
-            socket.emit('is-typing', true)
-            await nlu.process(data.value)
-          })
-
-          // Handle automatic speech recognition
-          socket.on('recognize', async (data) => {
-            try {
-              await asr.run(data, stt)
-            } catch (e) {
-              log[e.type](e.obj.message)
-            }
-          })
-        }
-
-        resolve()
-      })
+        })
+      }
     })
   }
 }
