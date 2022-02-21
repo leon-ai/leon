c65842591362121f332acad6fd6d5d93e86c4d83
louistiti <louis.grenard@gmail.com>
2022-02-21 23:13:26 +0800

    refactor(skill/who_am_i): from module to skill

---
+++ skills/leon/who_am_i/src/actions/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+
+def run(string, entities):
+	"""Leon introduces himself"""
+
+	return utils.output('end', 'introduction', utils.translate('introduction'))
