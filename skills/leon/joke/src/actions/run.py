195278f975c740f9e0d18151e92bd6ed4ea0ac4d
louistiti <louis.grenard@gmail.com>
2022-02-21 22:49:12 +0800

    refactor(skill/joke): from module to skill

---
+++ skills/leon/joke/src/actions/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+
+def run(string, entities):
+	"""Leon says some jokes"""
+
+	return utils.output('end', 'jokes', utils.translate('jokes'))
