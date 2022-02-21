13132279f393a877f4b1d55c0da4e671eaa1d7d2
louistiti <louis.grenard@gmail.com>
2022-02-21 22:52:24 +0800

    refactor(skill/meaning_of_life): from module to skill

---
+++ skills/leon/meaning_of_life/src/actions/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+
+def run(string, entities):
+	"""Leon says what's the meaning of life"""
+
+	return utils.output('end', 'meaning_of_life', utils.translate('meaning_of_life'))
