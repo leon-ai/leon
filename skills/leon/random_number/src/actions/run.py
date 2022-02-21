41c44369242a2d26a8689b7a04a702637c4ac045
louistiti <louis.grenard@gmail.com>
2022-02-21 23:06:08 +0800

    refactor(skill/random_number): from module to skill

---
+++ skills/leon/random_number/src/actions/run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+from random import randint
+
+def run(string, entities):
+	"""Leon gives a random number"""
+
+	return utils.output('end', 'success', randint(0, 100))
