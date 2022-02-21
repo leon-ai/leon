b19bb0e3eba901b7e7b2e97f9842ca901058260d
louistiti <louis.grenard@gmail.com>
2022-02-21 23:10:06 +0800

    refactor(skill/welcome): from module to skill

---
+++ skills/leon/welcome/src/actions/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+
+def run(string, entities):
+	"""Leon welcomes you"""
+
+	return utils.output('end', 'welcome', utils.translate('welcome'))
