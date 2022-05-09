0ebc0518e61b899c35dd13df65a43f69399e784d
louistiti <louis.grenard@gmail.com>
2022-05-09 22:31:47 +0800

    fix(skill/rochambeau): final logic

--- skills/games/rochambeau/src/actions/play.py
+++ skills/games/rochambeau/src/actions/play.py
@@ -9,44 +9,52 @@ def play(params):
 
 	handsigns = {
 		'ROCK': {
-			'superior_to': 'scissors',
-			'inferior_to': 'paper'
+			'superior_to': 'SCISSORS',
+			'inferior_to': 'PAPER'
 		},
 		'PAPER': {
-			'superior_to': 'rock',
-			'inferior_to': 'scissors'
+			'superior_to': 'ROCK',
+			'inferior_to': 'SCISSORS'
 		},
 		'SCISSORS': {
-			'superior_to': 'paper',
-			'inferior_to': 'rock'
+			'superior_to': 'PAPER',
+			'inferior_to': 'ROCK'
 		}
 	}
 	entities = params['entities']
 	rounds_nb = 3 # TODO: pickup from memory
-	player_handsign = None
-	leon_handsign = random.choice(list(handsigns))
+	player = {
+		'handsign': None,
+		'points': 0
+	}
+	leon = {
+		'handsign': random.choice(list(handsigns)),
+		'points': 0
+	}
 
 	# Find entities
 	for item in params['entities']:
 		if item['entity'] == 'handsign':
-			player_handsign = item['resolution']['value']
+			player['handsign'] = item['option']
 
 	# Return no speech if no number has been found
-	if player_handsign == None:
+	if player['handsign'] == None:
 		return utils.output('end', None, None, { 'isInActionLoop': False })
 
-	if leon_handsign == player_handsign:
+	if leon['handsign'] == player['handsign']:
 		return utils.output('end', 'equal', utils.translate('equal'))
 
 	# Point for Leon
-	if handsigns[leon_handsign]['superior_to'] == player_handsign:
+	if handsigns[leon['handsign']]['superior_to'] == player['handsign']:
 		# TODO: increment +1 for Leon
-		# TODO: remove print
-		print('...')
+		return utils.output('end', 'point_for_leon', utils.translate('point_for_leon', {
+			'handsign_1': leon['handsign'].lower(),
+			'handsign_2': player['handsign'].lower()
+		}))
 
 	# TODO: increment +1 for player
 
-	return utils.output('end', 'equal', utils.translate('point', {
-		'handsign_1': leon_handsign.lower(),
-		'handsign_2': player_handsign.lower()
+	return utils.output('end', 'point_for_player', utils.translate('point_for_player', {
+		'handsign_1': player['handsign'].lower(),
+		'handsign_2': leon['handsign'].lower()
 	}))
