ae4f69f7c7189ff75e004f68c9a2a8b6bb37b6bd
louistiti <louis.grenard@gmail.com>
2022-07-03 00:00:48 +0800

    feat(skill/mbti): questions mapping

--- skills/social_communication/mbti/src/actions/quiz.py
+++ skills/social_communication/mbti/src/actions/quiz.py
@@ -4,10 +4,34 @@
 import utils
 from ..lib import db
 
+counters = {
+	'mind': {
+		'E': 0, # Extraverted
+		'I': 0, # Introverted
+		'questions': [1, 5, 9, 13, 17]
+	},
+	'energy': {
+		'N': 0, # Intuitive
+		'S': 0, # Sensing
+		'questions': [2, 6, 10, 14, 18]
+	},
+	'nature': {
+		'T': 0, # Thinking
+		'F': 0, # Feeling
+		'questions': [3, 7, 11, 15, 19]
+	},
+	'tactics': {
+		'J': 0, # Judjing
+		'P': 0, # Perceiving
+		'questions': [4, 8, 12, 16, 20]
+	}
+}
+
 def quiz(params):
 	"""Loop over the questions and track choices"""
 
 	resolvers = params['resolvers']
+	choice = None
 
 	for resolver in resolvers:
 		if resolver['name'] == 'form':
