99a3f103e00b5a58745ee851d2fa95c61871f75a
louistiti <louis.grenard@gmail.com>
2022-07-03 12:17:05 +0800

    feat(skill/mbti): finish business logic

--- skills/social_communication/mbti/src/actions/quiz.py
+++ skills/social_communication/mbti/src/actions/quiz.py
@@ -1,52 +1,87 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+# Questions are taken from: http://www.lrjj.cn/encrm1.0/public/upload/MBTI-personality-test.pdf
+
 import utils
 from ..lib import db
 
-counters = {
-	'mind': {
-		'E': 0, # Extraverted
-		'I': 0, # Introverted
+groups = [
+	{
+		'name': 'mind',
+		'a': 'E', # Extraverted
+		'b': 'I', # Introverted
 		'questions': [1, 5, 9, 13, 17]
 	},
-	'energy': {
-		'N': 0, # Intuitive
-		'S': 0, # Sensing
+	{
+		'name': 'energy',
+		'a': 'S', # Sensing
+		'b': 'N', # Intuitive
 		'questions': [2, 6, 10, 14, 18]
 	},
-	'nature': {
-		'T': 0, # Thinking
-		'F': 0, # Feeling
+	{
+		'name': 'nature',
+		'a': 'T', # Thinking
+		'b': 'F', # Feeling
 		'questions': [3, 7, 11, 15, 19]
 	},
-	'tactics': {
-		'J': 0, # Judjing
-		'P': 0, # Perceiving
+	{
+		'name': 'tactics',
+		'a': 'J', # Judjing
+		'b': 'P', # Perceiving
 		'questions': [4, 8, 12, 16, 20]
 	}
-}
+]
 
 def quiz(params):
 	"""Loop over the questions and track choices"""
 
 	resolvers = params['resolvers']
 	choice = None
+	letter = None # E I S N T F
 
 	for resolver in resolvers:
 		if resolver['name'] == 'form':
 			choice = resolver['value']
 
+	# Return no speech if no value has been found
+	if choice == None:
+		return utils.output('end', None, { 'isInActionLoop': False })
+
+	question, choice = choice.split('_')
+
 	session = db.get_session()
-	current_question = session['current_question'] + 1
-	db.upsert_session(current_question)
+	current_question = session['current_question']
+	next_question = current_question + 1
 
+	# Define the letter to increment
+	for group in groups:
+		if current_question in group['questions']:
+			letter = group[choice]
+
+	db.increment_letter(letter)
+	db.upsert_session(next_question)
+
+	# Release final result
 	if current_question == 20:
-		# TODO
-		return utils.output('end', 'Your personality type is...', { 'isInActionLoop': False })
+		session_result = db.get_session()
+		type_arr = []
 
-	return utils.output('end', { 'key': str(current_question),
+		for group in groups:
+			group_letter = group['a'] if session_result[group['a']] >= session_result[group['b']] else group['b']
+			type_arr.append(group_letter)
+
+		final_type = ''.join(type_arr)
+
+		return utils.output('end', { 'key': 'result',
+			'data': {
+				'type': final_type,
+				'type_url': final_type.lower()
+			}
+		}, { 'isInActionLoop': False })
+
+	return utils.output('end', { 'key': str(next_question),
 		'data': {
-			'question': str(current_question)
+			'question': str(next_question)
 		}
 	})
--- skills/social_communication/mbti/src/lib/db.py
+++ skills/social_communication/mbti/src/lib/db.py
@@ -20,7 +20,26 @@ def upsert_session(current_question):
 		'current_question': current_question
 	}, doc_id=0))
 
+	if current_question == 1:
+		session_table.upsert(table.Document({
+			'E': 0,
+			'I': 0,
+			'S': 0,
+			'N': 0,
+			'T': 0,
+			'F': 0,
+			'J': 0,
+			'P': 0
+		}, doc_id=0))
+
+def increment_letter(letter):
+	"""Add one point to a letter"""
+
+	letter_score = get_session()[letter] + 1
+
+	session_table.update(table.Document({ letter: letter_score }, doc_id=0))
+
 def get_session():
-	"""TODO"""
+	"""Get current session"""
 
 	return session_table.get(doc_id=0)
