6d3d441fbfb844d3a5e54633e536b02c21298b8f
louistiti <louis.grenard@gmail.com>
2022-09-03 12:02:40 +0800

    feat: extend from main TypeScript config

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import server from '@/core/http-server/server'
 
   process.title = 'leon'
 
-  global.tcpServerProcess = spawn(`pipenv run python bridges/python/tcp_server/main.py ${lang.getShortCode(process.env.LEON_LANG)}`, { shell: true })
+  global.tcpServerProcess = spawn(`pipenv run python bridges/python/tcp_server/main.py ${lang.getShortCode(process.env['LEON_LANG'])}`, { shell: true })
 
   global.tcpClient = new TcpClient(
     process.env['LEON_PY_TCP_SERVER_HOST'],
