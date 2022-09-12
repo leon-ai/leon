da0233495d4b9abb93614ff1acd97c2c93f2b218
louistiti <louis.grenard@gmail.com>
2022-09-12 12:45:25 +0800

    refactor(server): replace Moment.js by Day.js

--- server/src/helpers/date.ts
+++ server/src/helpers/date.ts
@@ -1,16 +1,30 @@
-import moment from 'moment-timezone'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 import { TIME_ZONE } from '@/constants'
+import log from '@/helpers/log'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 export function getDateTime() {
-  return moment().tz(getTimeZone()).format()
+  return dayjs().tz(getTimeZone()).format()
 }
 
 export function getTimeZone() {
-  let timeZone = moment.tz.guess()
+  let { timeZone } = Intl.DateTimeFormat().resolvedOptions()
 
-  if (TIME_ZONE && !!moment.tz.zone(TIME_ZONE)) {
-    timeZone = TIME_ZONE
+  if (TIME_ZONE) {
+    // Verify if the time zone is valid
+    try {
+      Intl.DateTimeFormat(undefined, { timeZone: TIME_ZONE })
+      timeZone = TIME_ZONE
+    } catch (e) {
+      log.warning(
+        `The time zone "${TIME_ZONE}" is not valid. Falling back to "${timeZone}"`
+      )
+    }
   }
 
   return timeZone
