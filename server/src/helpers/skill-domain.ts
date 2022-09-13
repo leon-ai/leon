dabd657acfc01482fe6c2ab37d176bd94d0dcbb0
louistiti <louis.grenard@gmail.com>
2022-09-13 23:16:12 +0800

    refactor(server): add comments to the skill domain helper

--- server/src/helpers/skill-domain.ts
+++ server/src/helpers/skill-domain.ts
@@ -21,6 +21,9 @@ interface SkillDomain {
 
 const DOMAINS_DIR = path.join(process.cwd(), 'skills')
 
+/**
+ * List all skills domains with skills data inside
+ */
 export async function getSkillDomains() {
   const skillDomains = new Map<string, SkillDomain>()
 
@@ -67,12 +70,23 @@ export async function getSkillDomains() {
   return skillDomains
 }
 
+/**
+ * Get information of a specific domain
+ *
+ * @param domain Domain to get info from
+ */
 export function getSkillDomainInfo(domain: SkillDomain['name']) {
   return JSON.parse(
     fs.readFileSync(path.join(DOMAINS_DIR, domain, 'domain.json'), 'utf8')
   )
 }
 
+/**
+ * Get information of a specific skill
+ *
+ * @param domain Domain where the skill belongs
+ * @param skill Skill to get info from
+ */
 export function getSkillInfo(
   domain: SkillDomain['name'],
   skill: Skill['name']
