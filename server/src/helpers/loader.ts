e7cb31e70627643a88ec872db1a93378fc0fda0c
louistiti <louis.grenard@gmail.com>
2022-09-21 21:51:19 +0800

    chore(server): add comments on loader helper

--- server/src/helpers/loader.ts
+++ server/src/helpers/loader.ts
@@ -13,21 +13,18 @@ class Loader {
     'Go for a walk, this action takes time',
     "That may take some time, let's chill and relax",
     'Leon will be ready for you in a moment'
-  ] as const
+  ]
 
   private static instance: Loader
 
   private spinner = new Spinner('\x1b[95m%s\x1b[0m\r').setSpinnerString(18)
 
-  private interval: NodeJS.Timer | undefined = undefined
+  private interval: NodeJS.Timer | undefined
 
   private constructor() {
     // Singleton
   }
 
-  /**
-   * TODO
-   */
   public static getInstance() {
     if (Loader.instance == null) {
       Loader.instance = new Loader()
@@ -37,7 +34,7 @@ class Loader {
   }
 
   /**
-   * TODO
+   * Start the loader
    */
   public start() {
     this.interval = setInterval(() => {
@@ -53,7 +50,7 @@ class Loader {
   }
 
   /**
-   * TODO
+   * Stop the loader
    */
   public stop() {
     clearInterval(this.interval)
