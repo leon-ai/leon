25fdaeeab24c68c85c3318f60a5c560f476b9ae1
louistiti <louis.grenard@gmail.com>
2022-11-05 23:17:18 +0800

    refactor(web app): adapt new Vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,24 @@
+import dns from 'node:dns'
+
 import dotenv from 'dotenv'
+import { defineConfig } from 'vite'
 
 dotenv.config()
+
+dns.setDefaultResultOrder('verbatim')
 
 // Map necessary Leon's env vars as Vite only expose VITE_*
 process.env.VITE_LEON_NODE_ENV = process.env.LEON_NODE_ENV
 process.env.VITE_LEON_HOST = process.env.LEON_HOST
 process.env.VITE_LEON_PORT = process.env.LEON_PORT
 
-export default {
+export default defineConfig({
   root: 'app/src',
   build: {
     outDir: '../dist',
     emptyOutDir: true
+  },
+  server: {
+    port: 3000
   }
-}
+})
