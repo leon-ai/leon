f8b08f38b187f87748c35532a1ba9536d277c707
Divlo <contact@divlo.fr>
2022-11-13 12:46:31 +0100

    fix: stricter JSON Schemas
    - Type.Strict()
    - Type.Object({ additionalProperties: false })
    - Type.String({ format: 'email' | 'uri' })

--- server/src/schemas/global-data-schemas.ts
+++ server/src/schemas/global-data-schemas.ts
@@ -1,34 +1,57 @@
 import type { Static } from '@sinclair/typebox'
 import { Type } from '@sinclair/typebox'
 
-export const globalEntitySchemaObject = Type.Object({
-  options: Type.Record(
-    Type.String(),
-    Type.Object({
-      synonyms: Type.Array(Type.String()),
-      data: Type.Optional(Type.Record(Type.String(), Type.Array(Type.String())))
-    })
+export const globalEntitySchemaObject = Type.Strict(
+  Type.Object(
+    {
+      options: Type.Record(
+        Type.String(),
+        Type.Object(
+          {
+            synonyms: Type.Array(Type.String()),
+            data: Type.Optional(
+              Type.Record(Type.String(), Type.Array(Type.String()))
+            )
+          },
+          { additionalProperties: false }
+        )
+      )
+    },
+    { additionalProperties: false }
   )
-})
-export const globalResolverSchemaObject = Type.Object({
-  name: Type.String(),
-  intents: Type.Record(
-    Type.String(),
-    Type.Object({
-      utterance_samples: Type.Array(Type.String()),
-      value: Type.Unknown()
-    })
+)
+export const globalResolverSchemaObject = Type.Strict(
+  Type.Object(
+    {
+      name: Type.String(),
+      intents: Type.Record(
+        Type.String(),
+        Type.Object(
+          {
+            utterance_samples: Type.Array(Type.String()),
+            value: Type.Unknown()
+          },
+          { additionalProperties: false }
+        )
+      )
+    },
+    { additionalProperties: false }
   )
-})
-export const globalAnswersSchemaObject = Type.Object({
-  answers: Type.Record(
-    Type.String(),
-    Type.Union([
-      Type.Record(Type.String(), Type.String()),
-      Type.Array(Type.String())
-    ])
+)
+export const globalAnswersSchemaObject = Type.Strict(
+  Type.Object(
+    {
+      answers: Type.Record(
+        Type.String(),
+        Type.Union([
+          Type.Record(Type.String(), Type.String()),
+          Type.Array(Type.String())
+        ])
+      )
+    },
+    { additionalProperties: false }
   )
-})
+)
 
 export type GlobalEntity = Static<typeof globalEntitySchemaObject>
 export type GlobalResolver = Static<typeof globalResolverSchemaObject>
