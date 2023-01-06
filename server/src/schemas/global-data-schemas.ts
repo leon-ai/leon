31e44cde379704c0d38328462be02047898a0875
Divlo <contact@divlo.fr>
2023-01-06 22:20:55 +0100

    fix(server): improve schemas by adding descriptions

--- server/src/schemas/global-data-schemas.ts
+++ server/src/schemas/global-data-schemas.ts
@@ -16,38 +16,38 @@ export const globalEntitySchemaObject = Type.Strict(
           { additionalProperties: false }
         )
       )
+    },
+    {
+      description:
+        'Global entities can hold data that can directly be reused in skills.'
     }
   )
 )
 export const globalResolverSchemaObject = Type.Strict(
-  Type.Object(
-    {
-      name: Type.String(),
-      intents: Type.Record(
-        Type.String(),
-        Type.Object(
-          {
-            utterance_samples: Type.Array(Type.String()),
-            value: Type.Unknown()
-          },
-          { additionalProperties: false }
-        )
+  Type.Object({
+    name: Type.String(),
+    intents: Type.Record(
+      Type.String(),
+      Type.Object(
+        {
+          utterance_samples: Type.Array(Type.String()),
+          value: Type.Unknown()
+        },
+        { additionalProperties: false }
       )
-    }
-  )
+    )
+  })
 )
 export const globalAnswersSchemaObject = Type.Strict(
-  Type.Object(
-    {
-      answers: Type.Record(
-        Type.String(),
-        Type.Union([
-          Type.Record(Type.String(), Type.String()),
-          Type.Array(Type.String())
-        ])
-      )
-    }
-  )
+  Type.Object({
+    answers: Type.Record(
+      Type.String(),
+      Type.Union([
+        Type.Record(Type.String(), Type.String()),
+        Type.Array(Type.String())
+      ])
+    )
+  })
 )
 
 export type GlobalEntity = Static<typeof globalEntitySchemaObject>
