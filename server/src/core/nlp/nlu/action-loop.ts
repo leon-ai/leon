26ff4520c883485dcea2725174c99e8305c355f6
louistiti <louis.grenard@gmail.com>
2023-03-27 22:09:32 +0800

    refactor(server): action loop

--- server/src/core/nlp/nlu/action-loop.ts
+++ server/src/core/nlp/nlu/action-loop.ts
@@ -45,105 +45,116 @@ export class ActionLoop {
 
     const { actions, resolvers } = SkillDomainHelper.getSkillConfig(skillConfigPath, BRAIN.lang)
     const action = actions[NLU.nluResult.classification.action]
-    const { name: expectedItemName, type: expectedItemType } =
-      action.loop.expected_item
-    let hasMatchingEntity = false
-    let hasMatchingResolver = false
+    if (action?.loop) {
+      const { name: expectedItemName, type: expectedItemType } =
+        action.loop.expected_item
+      let hasMatchingEntity = false
+      let hasMatchingResolver = false
 
-    if (expectedItemType === 'entity') {
-      hasMatchingEntity =
-        NLU.nluResult.entities.filter(
-          ({ entity }) => expectedItemName === entity
-        ).length > 0
-    } else if (expectedItemType.indexOf('resolver') !== -1) {
-      const nlpObjs = {
-        global_resolver: MODEL_LOADER.globalResolversNLPContainer,
-        skill_resolver: MODEL_LOADER.skillsResolversNLPContainer
+      if (expectedItemType === 'entity') {
+        hasMatchingEntity =
+          NLU.nluResult.entities.filter(
+            ({ entity }) => expectedItemName === entity
+          ).length > 0
+      } else if (expectedItemType.indexOf('resolver') !== -1) {
+        const nlpObjs = {
+          global_resolver: MODEL_LOADER.globalResolversNLPContainer,
+          skill_resolver: MODEL_LOADER.skillsResolversNLPContainer
+        }
+        const result = await nlpObjs[expectedItemType].process(utterance)
+        const { intent } = result
+
+        const resolveResolvers = (
+          resolver: string,
+          intent: string
+        ): [{ name: string, value: string }] => {
+          const resolversPath = join(
+            process.cwd(),
+            'core',
+            'data',
+            BRAIN.lang,
+            'global-resolvers'
+          )
+          // Load the skill resolver or the global resolver
+          const resolvedIntents = !intent.includes('resolver.global')
+            ? (resolvers && resolvers[resolver])
+            : JSON.parse(fs.readFileSync(join(resolversPath, `${resolver}.json`), 'utf8'))
+
+          // E.g. resolver.global.denial -> denial
+          intent = intent.substring(intent.lastIndexOf('.') + 1)
+
+          return [
+            {
+              name: expectedItemName,
+              value: resolvedIntents.intents[intent].value
+            }
+          ]
+        }
+
+        // Resolve resolver if global resolver or skill resolver has been found
+        if (
+          intent &&
+          (intent.includes('resolver.global') ||
+            intent.includes(`resolver.${skillName}`))
+        ) {
+          LogHelper.title('NLU')
+          LogHelper.success('Resolvers resolved:')
+          NLU.nluResult.resolvers = resolveResolvers(expectedItemName, intent)
+          NLU.nluResult.resolvers.forEach((resolver) =>
+            LogHelper.success(`${intent}: ${JSON.stringify(resolver)}`)
+          )
+          hasMatchingResolver = NLU.nluResult.resolvers.length > 0
+        }
       }
-      const result = await nlpObjs[expectedItemType].process(utterance)
-      const { intent } = result
 
-      const resolveResolvers = (resolver, intent) => {
-        const resolversPath = join(
-          process.cwd(),
-          'core',
-          'data',
-          BRAIN.lang,
-          'global-resolvers'
-        )
-        // Load the skill resolver or the global resolver
-        const resolvedIntents = !intent.includes('resolver.global')
-          ? resolvers[resolver]
-          : JSON.parse(fs.readFileSync(join(resolversPath, `${resolver}.json`)))
+      // Ensure expected items are in the utterance, otherwise clean context and reprocess
+      if (!hasMatchingEntity && !hasMatchingResolver) {
+        BRAIN.talk(`${BRAIN.wernicke('random_context_out_of_topic')}.`)
+        NLU.conversation.cleanActiveContext()
+        await NLU.process(utterance)
+        return null
+      }
 
-        // E.g. resolver.global.denial -> denial
-        intent = intent.substring(intent.lastIndexOf('.') + 1)
+      try {
+        const processedData = await BRAIN.execute(NLU.nluResult)
+        // Reprocess with the original utterance that triggered the context at first
+        if (processedData.core?.restart === true) {
+          const { originalUtterance } = NLU.conversation.activeContext
 
-        return [
-          {
-            name: expectedItemName,
-            value: resolvedIntents.intents[intent].value
+          NLU.conversation.cleanActiveContext()
+
+          if (originalUtterance !== null) {
+            await NLU.process(originalUtterance)
           }
-        ]
-      }
 
-      // Resolve resolver if global resolver or skill resolver has been found
-      if (
-        intent &&
-        (intent.includes('resolver.global') ||
-          intent.includes(`resolver.${skillName}`))
-      ) {
-        LogHelper.title('NLU')
-        LogHelper.success('Resolvers resolved:')
-        NLU.nluResult.resolvers = resolveResolvers(expectedItemName, intent)
-        NLU.nluResult.resolvers.forEach((resolver) =>
-          LogHelper.success(`${intent}: ${JSON.stringify(resolver)}`)
-        )
-        hasMatchingResolver = NLU.nluResult.resolvers.length > 0
-      }
-    }
+          return null
+        }
 
-    // Ensure expected items are in the utterance, otherwise clean context and reprocess
-    if (!hasMatchingEntity && !hasMatchingResolver) {
-      BRAIN.talk(`${BRAIN.wernicke('random_context_out_of_topic')}.`)
-      NLU.conversation.cleanActiveContext()
-      await NLU.process(utterance)
-      return null
-    }
+        /**
+         * In case there is no next action to prepare anymore
+         * and there is an explicit stop of the loop from the skill
+         */
+        if (
+          !processedData.action?.next_action &&
+          processedData.core?.isInActionLoop === false
+        ) {
+          NLU.conversation.cleanActiveContext()
+          return null
+        }
 
-    try {
-      const processedData = await BRAIN.execute(NLU.nluResult)
-      // Reprocess with the original utterance that triggered the context at first
-      if (processedData.core?.restart === true) {
-        const { originalUtterance } = NLU.conversation.activeContext
+        // Break the action loop and prepare for the next action if necessary
+        if (processedData.core?.isInActionLoop === false) {
+          NLU.conversation.activeContext.isInActionLoop = !!processedData.action?.loop
+          NLU.conversation.activeContext.actionName = processedData.action?.next_action as string
+          NLU.conversation.activeContext.intent = `${processedData.classification?.skill}.${processedData.action?.next_action}`
+        }
 
-        NLU.conversation.cleanActiveContext()
-        await NLU.process(originalUtterance)
+        return processedData
+      } catch (e) {
         return null
       }
-
-      /**
-       * In case there is no next action to prepare anymore
-       * and there is an explicit stop of the loop from the skill
-       */
-      if (
-        !processedData.action.next_action &&
-        processedData.core?.isInActionLoop === false
-      ) {
-        NLU.conversation.cleanActiveContext()
-        return null
-      }
-
-      // Break the action loop and prepare for the next action if necessary
-      if (processedData.core?.isInActionLoop === false) {
-        NLU.conversation.activeContext.isInActionLoop = !!processedData.action.loop
-        NLU.conversation.activeContext.actionName = processedData.action.next_action
-        NLU.conversation.activeContext.intent = `${processedData.classification.skill}.${processedData.action.next_action}`
-      }
-
-      return processedData
-    } catch (e) {
-      return null
     }
+
+    return null
   }
 }
