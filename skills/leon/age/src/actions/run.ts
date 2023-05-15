d3ba85cb360fe17b2b16dc4994c936d1f40d262f
Divlo <contact@divlo.fr>
2023-05-15 21:18:54 +0200

    feat(bridge/nodejs): add Widget abstract class

--- skills/leon/age/src/actions/run.ts
+++ skills/leon/age/src/actions/run.ts
@@ -2,7 +2,6 @@ import utility from 'utility' // TODO
 
 import type { ActionFunction } from '@sdk/types'
 import { leon } from '@sdk/leon'
-import { Widget } from '@sdk/widget'
 import { Network } from '@sdk/network'
 import { Button } from '@sdk/aurora/button'
 import { Memory } from '@sdk/memory'
@@ -21,9 +20,7 @@ export const run: ActionFunction = async function () {
   const button = new Button({
     text: 'Hello world from action skill'
   })
-  const widget = new Widget([button])
-
-  await leon.answer({ widget })
+  await leon.answer({ widget: button })
 
   ///
 
