623039415ad8aeadda2cbc249b0b6ffe475c1aea
Divlo <contact@divlo.fr>
2023-05-15 21:18:54 +0200

    feat(bridge/nodejs): add Widget abstract class

--- bridges/nodejs/src/sdk/widget.ts
+++ bridges/nodejs/src/sdk/widget.ts
@@ -1,23 +1,9 @@
-import type { AuroraComponent } from '@sdk/types'
+export abstract class Widget<T> {
+  public readonly type: string
+  public readonly options: T
 
-interface SerializedAuroraComponent {
-  type: string
-  props: Record<string, unknown>
-}
-
-export class Widget {
-  private readonly components: SerializedAuroraComponent[]
-
-  constructor(components: AuroraComponent[]) {
-    this.components = components.map((component) => {
-      return {
-        type: component.constructor.name,
-        props: {
-          ...component
-        }
-      }
-    })
-
-    console.log('Widget constructor', this.components)
+  public constructor(options: T) {
+    this.type = this.constructor.name
+    this.options = options
   }
 }
