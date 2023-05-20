289efb5610adbc160058ae4f8af0be8365b5df8e
louistiti <louis.grenard@gmail.com>
2023-05-20 22:49:17 +0800

    feat(bridge/python): `leon.get_src_config`

--- bridges/python/src/sdk/leon.py
+++ bridges/python/src/sdk/leon.py
@@ -5,4 +5,14 @@ class Leon:
 		if not Leon.instance:
 			Leon.instance = self
 
+	def get_src_config(key: str = None) -> T:
+        try:
+            if key:
+                return SKILL_SRC_CONFIG[key]
+
+            return SKILL_SRC_CONFIG
+        except Exception as e:
+            print('Error while getting source configuration:', e)
+            return {}
+
 leon = Leon()
