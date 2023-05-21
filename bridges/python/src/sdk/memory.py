3a230633d823e4b058a55b3a0db141c021d33b8a
louistiti <louis.grenard@gmail.com>
2023-05-21 19:12:30 +0800

    feat(bridge/python): memory kick off

---
+++ bridges/python/src/sdk/memory.py
@@ -0,0 +1,30 @@
+# TODO
+
+import os.path
+
+from ..constants import SKILL_PATH, SKILLS_PATH
+
+
+class Memory:
+    def __init__(self, options):
+        self.name = options.name
+        self.default_memory = options.default_memory
+
+        if ':' in options.name and options.name.count(':') == 3:
+            domain_name, skill_name, memory_name = options.name.split(':')
+            self.memory_path = os.path.join(
+                SKILLS_PATH,
+                domain_name,
+                skill_name,
+                'memory',
+                memory_name + '.json'
+            )
+            if os.path.exists(self.memory_path):
+                # TODO
+                pass
+        else:
+            self.memory_path = os.path.join(
+                SKILL_PATH,
+                'memory',
+                options.name + '.json'
+            )
