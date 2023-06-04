305d9cd581dc8c3e1c57c6c8b538e122dffbb382
louistiti <louis.grenard@gmail.com>
2023-06-04 17:06:24 +0800

    feat(skill/widget-playground): first widget components draft structure

--- skills/unknown/widget-playground/src/actions/run.ts
+++ skills/unknown/widget-playground/src/actions/run.ts
@@ -1,7 +1,99 @@
 import type { ActionFunction } from '@sdk/types'
 import { leon } from '@sdk/leon'
 
+/**
+ * Aurora component props
+ */
+
+interface Text {
+  text: string
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' // TODO: design tokens ("md" default)
+  type?: 'primary' | 'secondary' // TODO: design tokens ("primary" default)
+}
+interface List {
+  title: {
+    text: string
+    align?: 'left' | 'center' | 'right' // TODO: design tokens ("left" default)
+  }
+}
+interface Container {
+  direction?: 'row' | 'column' // TODO: design tokens ("row" default)
+  align?: 'left' | 'center' | 'right' // TODO: design tokens
+}
+interface Image {
+  path: string
+  width?: string
+  height?: string
+}
+interface Checkbox {
+  checked: boolean
+  label?: string
+  disabled?: boolean
+  hint?: string
+  onChange?: () => void
+}
+interface Input {
+  value: string
+  placeholder?: string
+  disabled?: boolean
+  onChange?: () => void
+}
+
 export const run: ActionFunction = async function () {
+  /**
+   * Select music provider
+   */
+
+  const musicProviderList = new List({
+    title: {
+      text: 'Select your music provider',
+      align: 'center'
+    },
+    items: [
+      {
+        content: new Container(
+          [
+            new Image({ path: 'spotify.svg', width: '', height: '' }) // TODO: props
+          ],
+          {
+            align: 'center'
+          }
+        ),
+        onPress: async () => {
+          // TODO: next step
+        }
+      }
+    ]
+  })
+
+  /**
+   * Todo list
+   */
+  const list = new List({
+    title: {
+      text: 'Shopping List',
+      align: 'left'
+    },
+    items: [
+      {
+        content: new Container([
+          new Checkbox({
+            isChecked: true,
+            onChange: () => {
+              // TODO: toggle todo
+            }
+          }),
+          new Input({
+            value: 'Milk',
+            onChange: () => {
+              // TODO: open modal/dialog to edit current todo
+            }
+          })
+        ])
+      }
+    ]
+  })
+
   /**
    * Random number
    */
