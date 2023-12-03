ca59f02f4f032670d361d93658cfcf3560a48b67
louistiti <louis.grenard@gmail.com>
2023-12-03 22:00:50 +0800

    feat(bridge/nodejs): add `ScrollContainer` component

---
+++ bridges/nodejs/src/sdk/aurora/scroll-container.ts
@@ -0,0 +1,9 @@
+import { type ScrollContainerProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class ScrollContainer extends Widget<ScrollContainerProps> {
+  constructor(props: ScrollContainerProps) {
+    super(props)
+  }
+}
