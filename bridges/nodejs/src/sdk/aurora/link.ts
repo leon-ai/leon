d460dc59f52cc45244df29d28df389282dfecb30
louistiti <louis.grenard@gmail.com>
2023-12-03 21:46:47 +0800

    feat(bridge/nodejs): add `Link` component

---
+++ bridges/nodejs/src/sdk/aurora/link.ts
@@ -0,0 +1,9 @@
+import { type LinkProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Link extends Widget<LinkProps> {
+  constructor(props: LinkProps) {
+    super(props)
+  }
+}
