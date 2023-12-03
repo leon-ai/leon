1f0e4d2260f1f442797f6274a6decd975959622d
louistiti <louis.grenard@gmail.com>
2023-12-03 21:47:41 +0800

    feat(bridge/nodejs): add `List` component

---
+++ bridges/nodejs/src/sdk/aurora/list.ts
@@ -0,0 +1,9 @@
+import { type ListProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class List extends Widget<ListProps> {
+  constructor(props: ListProps) {
+    super(props)
+  }
+}
