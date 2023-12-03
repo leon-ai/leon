7af761c5f6b902768bd0c4fc421cdee42e56da29
louistiti <louis.grenard@gmail.com>
2023-12-03 22:00:11 +0800

    feat(bridge/nodejs): add `RadioGroup` component

---
+++ bridges/nodejs/src/sdk/aurora/radio-group.ts
@@ -0,0 +1,9 @@
+import { type RadioGroupProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class RadioGroup extends Widget<RadioGroupProps> {
+  constructor(props: RadioGroupProps) {
+    super(props)
+  }
+}
