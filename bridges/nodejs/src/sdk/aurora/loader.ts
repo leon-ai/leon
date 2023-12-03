90b5596b76338be07e9f5ed4474e4100edb77d3e
louistiti <louis.grenard@gmail.com>
2023-12-03 21:58:01 +0800

    feat(bridge/nodejs): add `Loader` component

---
+++ bridges/nodejs/src/sdk/aurora/loader.ts
@@ -0,0 +1,9 @@
+import { type LoaderProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Loader extends Widget<LoaderProps> {
+  constructor(props: LoaderProps) {
+    super(props)
+  }
+}
