eb646956971410bcebb7557cb002075a1ba810ad
louistiti <louis.grenard@gmail.com>
2023-12-03 21:44:00 +0800

    feat(bridge/nodejs): add `TextInput` component

---
+++ bridges/nodejs/src/sdk/aurora/text-input.ts
@@ -0,0 +1,9 @@
+import { type TextInputProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class TextInput extends Widget<TextInputProps> {
+  constructor(props: TextInputProps) {
+    super(props)
+  }
+}
