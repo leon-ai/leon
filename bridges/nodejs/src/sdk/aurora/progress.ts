3bf457caefac3a2df90c4076ed10d04aa729ee7b
louistiti <louis.grenard@gmail.com>
2023-12-03 21:58:55 +0800

    feat(bridge/nodejs): add `Progress` component

---
+++ bridges/nodejs/src/sdk/aurora/progress.ts
@@ -0,0 +1,9 @@
+import { type ProgressProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Progress extends Widget<ProgressProps> {
+  constructor(props: ProgressProps) {
+    super(props)
+  }
+}
