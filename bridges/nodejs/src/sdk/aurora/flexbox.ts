2cf4ef54c3f928a264bbfac3cb7db551a0accde1
louistiti <louis.grenard@gmail.com>
2023-12-03 21:38:36 +0800

    feat(bridge/nodejs): add `Flexbox` component

---
+++ bridges/nodejs/src/sdk/aurora/flexbox.ts
@@ -0,0 +1,9 @@
+import { type FlexboxProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Flexbox extends Widget<FlexboxProps> {
+  constructor(props: FlexboxProps) {
+    super(props)
+  }
+}
