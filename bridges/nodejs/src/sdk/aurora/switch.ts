14e5bee7d6051ea5225a1501a81f108119a6abe6
louistiti <louis.grenard@gmail.com>
2023-12-03 22:03:53 +0800

    feat(bridge/nodejs): add `Switch` component

---
+++ bridges/nodejs/src/sdk/aurora/switch.ts
@@ -0,0 +1,9 @@
+import { type SwitchProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Switch extends Widget<SwitchProps> {
+  constructor(props: SwitchProps) {
+    super(props)
+  }
+}
