34a5e8bac16ba7076cf0cac4963f5234105f4f99
louistiti <louis.grenard@gmail.com>
2023-12-03 21:37:10 +0800

    feat(bridge/nodejs): add `CircularProgress` component

---
+++ bridges/nodejs/src/sdk/aurora/circular-progress.ts
@@ -0,0 +1,9 @@
+import { type CircularProgressProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class CircularProgress extends Widget<CircularProgressProps> {
+  constructor(props: CircularProgressProps) {
+    super(props)
+  }
+}
--- bridges/nodejs/src/sdk/aurora/index.ts
+++ bridges/nodejs/src/sdk/aurora/index.ts
@@ -1,4 +1,5 @@
 export * from './button'
 export * from './card'
 export * from './checkbox'
+export * from './circular-progress'
 export * from './widget-wrapper'
