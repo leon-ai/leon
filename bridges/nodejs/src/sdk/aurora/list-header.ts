bad92d3569e4b7757bc64467643bfc441970804f
louistiti <louis.grenard@gmail.com>
2023-12-03 21:48:23 +0800

    feat(bridge/nodejs): add `ListHeader` component

---
+++ bridges/nodejs/src/sdk/aurora/list-header.ts
@@ -0,0 +1,9 @@
+import { type ListHeaderProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class ListHeader extends Widget<ListHeaderProps> {
+  constructor(props: ListHeaderProps) {
+    super(props)
+  }
+}
