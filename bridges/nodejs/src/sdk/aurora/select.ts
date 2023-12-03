8cb0816f24820f8aeac2e3e2e0a98d57f6744278
louistiti <louis.grenard@gmail.com>
2023-12-03 22:01:28 +0800

    feat(bridge/nodejs): add `Select` component

---
+++ bridges/nodejs/src/sdk/aurora/select.ts
@@ -0,0 +1,9 @@
+import { type SelectProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Select extends Widget<SelectProps> {
+  constructor(props: SelectProps) {
+    super(props)
+  }
+}
