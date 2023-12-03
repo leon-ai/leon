308bebeb4882fa51b937db718676e488fae12f1b
louistiti <louis.grenard@gmail.com>
2023-12-03 21:41:08 +0800

    feat(bridge/nodejs): add `Image` component

---
+++ bridges/nodejs/src/sdk/aurora/image.ts
@@ -0,0 +1,9 @@
+import { type ImageProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Image extends Widget<ImageProps> {
+  constructor(props: ImageProps) {
+    super(props)
+  }
+}
