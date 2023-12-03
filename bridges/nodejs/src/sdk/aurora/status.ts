2576987c44467da32967717fff804ea60062a770
louistiti <louis.grenard@gmail.com>
2023-12-03 22:03:15 +0800

    feat(bridge/nodejs): add `Status` component

---
+++ bridges/nodejs/src/sdk/aurora/status.ts
@@ -0,0 +1,9 @@
+import { type StatusProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Status extends Widget<StatusProps> {
+  constructor(props: StatusProps) {
+    super(props)
+  }
+}
