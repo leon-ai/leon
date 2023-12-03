b55d58ec3091992a4f3322bc6d94d1c184a6d415
louistiti <louis.grenard@gmail.com>
2023-12-03 21:39:34 +0800

    feat(bridge/nodejs): add `Icon` component

---
+++ bridges/nodejs/src/sdk/aurora/icon.ts
@@ -0,0 +1,9 @@
+import { type IconProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Icon extends Widget<IconProps> {
+  constructor(props: IconProps) {
+    super(props)
+  }
+}
