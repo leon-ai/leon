ea7d6c48db66b5a145693289b72e17a476c67ffa
louistiti <louis.grenard@gmail.com>
2023-12-03 22:02:37 +0800

    feat(bridge/nodejs): add `Slider` component

---
+++ bridges/nodejs/src/sdk/aurora/slider.ts
@@ -0,0 +1,9 @@
+import { type SliderProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Slider extends Widget<SliderProps> {
+  constructor(props: SliderProps) {
+    super(props)
+  }
+}
