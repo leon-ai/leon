bd71866342b9a361133fa970b51d0709062f58b6
louistiti <louis.grenard@gmail.com>
2023-12-03 21:40:21 +0800

    feat(bridge/nodejs): add `IconButton` component

---
+++ bridges/nodejs/src/sdk/aurora/icon-button.ts
@@ -0,0 +1,9 @@
+import { type IconButtonProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class IconButton extends Widget<IconButtonProps> {
+  constructor(props: IconButtonProps) {
+    super(props)
+  }
+}
