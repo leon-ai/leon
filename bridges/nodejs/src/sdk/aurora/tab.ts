64e1e7da3e3dde11446080a6eefe853cc9432d93
louistiti <louis.grenard@gmail.com>
2023-12-03 22:04:27 +0800

    feat(bridge/nodejs): add `Tab` component

---
+++ bridges/nodejs/src/sdk/aurora/tab.ts
@@ -0,0 +1,9 @@
+import { type TabProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Tab extends Widget<TabProps> {
+  constructor(props: TabProps) {
+    super(props)
+  }
+}
