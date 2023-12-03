f2b75fc184e9150a5264685283387bd58faa4d3e
louistiti <louis.grenard@gmail.com>
2023-12-03 21:59:41 +0800

    feat(bridge/nodejs): add `Radio` component

---
+++ bridges/nodejs/src/sdk/aurora/radio.ts
@@ -0,0 +1,9 @@
+import { type RadioProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Radio extends Widget<RadioProps> {
+  constructor(props: RadioProps) {
+    super(props)
+  }
+}
