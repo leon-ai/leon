bba60ea45d68cb5444e5c03075781d97410edc44
louistiti <louis.grenard@gmail.com>
2023-12-03 22:05:02 +0800

    feat(bridge/nodejs): add `TabContent` component

---
+++ bridges/nodejs/src/sdk/aurora/tab-content.ts
@@ -0,0 +1,9 @@
+import { type TabContentProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class TabContent extends Widget<TabContentProps> {
+  constructor(props: TabContentProps) {
+    super(props)
+  }
+}
