1408f358c7fffcf60ab4bd866ac0f0a2c16647cd
louistiti <louis.grenard@gmail.com>
2023-12-03 22:07:12 +0800

    feat(bridge/nodejs): add `Text` component

---
+++ bridges/nodejs/src/sdk/aurora/text.ts
@@ -0,0 +1,9 @@
+import { type TextProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Text extends Widget<TextProps> {
+  constructor(props: TextProps) {
+    super(props)
+  }
+}
