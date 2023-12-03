502ec8ba2ecccd4bd15817f9f6e31fbba0ae8db5
louistiti <louis.grenard@gmail.com>
2023-12-03 21:35:56 +0800

    feat(bridge/nodejs): add `Checkbox` component

---
+++ bridges/nodejs/src/sdk/aurora/checkbox.ts
@@ -0,0 +1,9 @@
+import { type CheckboxProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class Checkbox extends Widget<CheckboxProps> {
+  constructor(props: CheckboxProps) {
+    super(props)
+  }
+}
--- bridges/nodejs/src/sdk/aurora/index.ts
+++ bridges/nodejs/src/sdk/aurora/index.ts
@@ -1,3 +1,4 @@
 export * from './button'
 export * from './card'
+export * from './checkbox'
 export * from './widget-wrapper'
