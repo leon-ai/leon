10a17b0146dcf5ea9790bece53a1f3354857ba42
louistiti <louis.grenard@gmail.com>
2023-12-03 22:05:52 +0800

    feat(bridge/nodejs): add `TabGroup` component

---
+++ bridges/nodejs/src/sdk/aurora/tab-group.ts
@@ -0,0 +1,9 @@
+import { type TabGroupProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class TabGroup extends Widget<TabGroupProps> {
+  constructor(props: TabGroupProps) {
+    super(props)
+  }
+}
