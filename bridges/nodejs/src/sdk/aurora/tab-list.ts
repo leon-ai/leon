0297e1d22b3e0782c1446f2186af030b5ac87d00
louistiti <louis.grenard@gmail.com>
2023-12-03 22:06:30 +0800

    feat(bridge/nodejs): add `TabList` component

---
+++ bridges/nodejs/src/sdk/aurora/tab-list.ts
@@ -0,0 +1,9 @@
+import { type TabListProps } from '@leon-ai/aurora'
+
+import { Widget } from '../widget'
+
+export class TabList extends Widget<TabListProps> {
+  constructor(props: TabListProps) {
+    super(props)
+  }
+}
